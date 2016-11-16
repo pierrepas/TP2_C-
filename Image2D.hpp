1bfeb5028c6b87cebb5dbbc05b4f84d361609214
PierrePas <pierrepas@yahoo.fr>
2016-11-16 14:32:56 +0100

    Ajout makefile

--- Image2D.hpp
+++ Image2D.hpp
@@ -4,38 +4,54 @@
 /// Classe générique pour représenter des images 2D.
 template <typename TValue>
 class Image2D {
-	public:
-		typedef Image2D<TValue>    Self;      // le type de *this
-		typedef TValue             Value;     // le type pour la valeur des pixels
-		typedef std::vector<Value> Container; // le type pour stocker les valeurs des pixels de l'image.
-		// Constructeur par défaut
-		Image2D();
-		// Constructeur avec taille w x h. Remplit tout avec la valeur g
-		// (par défaut celle donnée par le constructeur par défaut).
-		Image2D( int w, int h, Value g = Value() );
+    public:
+        typedef Image2D<TValue>    Self;      // le type de *this
+        typedef TValue             Value;     // le type pour la valeur des pixels
+        typedef std::vector<Value> Container; // le type pour stocker les valeurs des pixels de l'image.
+        // Constructeur par défaut
+        Image2D();
+        // Constructeur avec taille w x h. Remplit tout avec la valeur g
+        // (par défaut celle donnée par le constructeur par défaut).
+        Image2D( int w, int h, Value g = Value() ){
+            m_data.resize( w * h );
+            m_width = w;
+            m_height = h;
+            fill(g);
+        }
 
-		// Remplit l'image avec la valeur \a g.
-		void fill( Value g );
+        // Remplit l'image avec la valeur \a g.
+        void fill(TValue g){
+            for(int i = 0; i < w(); i++){
+                for(int j = 0; j< h(); j++){
+                    at( i, j) = g;
+                }
+            }
+        }
 
-		/// @return la largeur de l'image.
-		int w() const;
-		/// @return la hauteur de l'image.
-		int h() const;
+        /// @return la largeur de l'image.
+        int w() const { return m_width; }
+        /// @return la hauteur de l'image.
+        int h() const { return m_height; }
 
-		/// Accesseur read-only à la valeur d'un pixel.
-		/// @return la valeur du pixel(i,j)
-		Value at( int i, int j ) const;
+        /// Accesseur read-only à la valeur d'un pixel.
+        /// @return la valeur du pixel(i,j)
+        Value at( int i, int j ) const{
+            return m_data[index(i,j)];
+        }
+        /// Accesseur read-write à la valeur d'un pixel.
+        /// @return une référence à la valeur du pixel(i,j)
+        Value& at( int i, int j ){
+            return m_data[index(i,j)];
+        }
 
-		/// Accesseur read-write à la valeur d'un pixel.
-		/// @return une référence à la valeur du pixel(i,j)
-		Value& at( int i, int j );
+    private:
+        Container m_data; // mes données; évitera de faire les allocations dynamiques
+        int m_width; // ma largeur
+        int m_height; // ma hauteur
 
-	private:
-		Container m_data; // mes données; évitera de faire les allocations dynamiques
-		int m_width; // ma largeur
-		int m_height; // ma hauteur
-
-		/// @return l'index du pixel (x,y) dans le tableau \red m_data.
-		int index( int i, int j ) const;
+        /// @return l'index du pixel (x,y) dans le tableau \red m_data.
+        int index( int x, int y ) const{
+            return x + w() * y;
+        }
 };
 #endif // _IMAGE2D_HPP_
